adf7a5bb5c9f87d37265f02377217b7c45acfb63
Admin <admin@Admins-MacBook-Pro-3.local>
2016-12-21 12:32:34 -0800

    Initial Commit

---
+++ NotificationObserver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotificationObserver
+//
+//  Created by Admin on 12/21/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotificationObserver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotificationObserver
+//
+//  Created by Admin on 12/21/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
